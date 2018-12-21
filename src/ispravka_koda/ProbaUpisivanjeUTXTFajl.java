72fd80985d630503ecfef76333706d862e0e4f8c
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-21 19:24:47 +0100

    Izmenjen naziv klase za pozivanje metode u ispravci koda.

--- src/ispravka_koda/ProbaUpisivanjeUTXTFajl.java
+++ src/ispravka_koda/ProbaUpisivanjeUTXTFajl.java
@@ -1,6 +1,6 @@
 package ispravka_koda;
 
-public class TestUpisivanjeUTXTFajl {
+public class ProbaUpisivanjeUTXTFajl {
 
 	public static void main(String[] args) {
 		UpisivanjeUTXTFajl.upisiProsteBrojeve();
